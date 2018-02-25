17cdfbcfb3d206fe649cde475beb3f09e50b4bb0
henriettao <henriettaodiete@rocketmail.com>
2018-02-25 09:32:44 -0500

    Update feb21_1_test.m
    adding comments to feb21_1.m

--- feb21_1_test.m
+++ feb21_1_test.m
@@ -1,44 +1,35 @@
 clc 
 clear all;
 close all; 
-% Manual Mixing test with RICA algorithm. 
+% Manual Mixing test with fastICA algorithm. 
 %% case 1 
-[Audio1, Fs1]= audioread('Audio_Recordings/7-laptop_mic3.wav');
-[Audio2, Fs2]= audioread('Audio_Recordings/9-headphone_mic3.wav');
+% read audio files from file path 
+[Audio1, Fs1]= audioread('recording1.wav');
+[Audio2, Fs2]= audioread('recording1.wav');
 
+% find the minimum length and trim length of longer file 
 L=min(0.5e6, min(length(Audio1), length(Audio2)));
 Audio1 = Audio1(1:L);
 Audio2 = Audio2(1:L);
+% add trimmed audio to a 2xm matrix where m is the length L 
 X(1,:)= Audio1;
 X(2,:)= Audio2;
+
+% run fastICA to recover independent components 
 [SE, W, A]=fastica(X, 'approach', 'symm', 'numOfIC', '2', 'g','tanh', 'epsilon','0.000001', 'maxNumIterations','1000', 'maxFineTune','1000');
 
+
+% makes new directory to store output from fastICA 
 mkdir feb22_case1
 cd
 cd feb22_case1
-case1_output1 ='case1_output9.wav'; 
-case1_output2 ='case1_output10.wav';
+case1_output1 ='case1_output#.wav'; 
+case1_output2 ='case1_output#.wav';
+
+% writes output file to .wav files and saves it 
 audiowrite(case1_output1, SE(1,:),Fs1);
 audiowrite(case1_output2, SE(2,:),Fs2);
 
 
-%% case 2 
 
-% [Audio1, Fs1]= audioread('C1 2/C1-Henr.wav');
-% [Audio2, Fs2]= audioread('C1 2/C1-Kyle.wav');
-% 
-% L=min(0.5e6, min(length(Audio1), length(Audio2)));
-% Audio1 = Audio1(1:L);
-% Audio2 = Audio2(1:L);
-% X(1,:)= Audio1;
-% X(2,:)= Audio2;
-% [SE, W, A]=fastica(X, 'approach', 'symm', 'numOfIC', '2', 'g','tanh', 'epsilon','0.000001');
-% 
-% mkdir feb22_case2
-% cd
-% cd feb22_case1
-% case1_output1 ='case1_output1.wav'; 
-% case1_output2 ='case1_output2.wav';
-% audiowrite(case1_output1, SE(1,:),Fs1);
-% audiowrite(case1_output2, SE(2,:),Fs2);
 
